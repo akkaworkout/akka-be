438bbd28c376f63a9ca76f9f48640599a741874c
minju0236 <minju0236@gmail.com>
2026-01-24 00:31:34 +0900

    ✨feat: swagger 설정 및 서버 구조 정리

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello Node Backend");
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /ping:
+ *   get:
+ *     summary: 서버 상태 확인
+ *     description: 서버가 살아있는지 확인하는 테스트 API
+ *     responses:
+ *       200:
+ *         description: pong 반환
+ */
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
 module.exports = app;
