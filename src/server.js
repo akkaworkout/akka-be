94522620925dce37d3c4fe3747e6554e63922a62
minju0236 <minju0236@gmail.com>
2026-01-31 23:08:05 +0900

    🐛 fix: Express 서버 listen 호스트를 0.0.0.0으로 변경

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`서버 실행 중 PORT: ${PORT}`);
 });
