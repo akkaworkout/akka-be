565d2748c5b31ea109cdcd08c12d8bdcf3c3555d
tjsgpals <tjsgpals@tjsgpalsui-MacBookAir.local>
2026-03-13 18:03:25 +0900

    feat: ⚙️사이드바 프로필 이미지 업로드 수정”

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.set("trust proxy", 1);
 /* CORS */
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:3000",
   "https://akkaworkout.store"
 ];
